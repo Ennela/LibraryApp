63b67d7d86bdf0dc0da0643c2e81c489d2e0a889
Ennela <remkyorosi@gmail.com>
2025-10-17 09:07:33 +0700

    add gitignore

---
+++ LibraryApp/Data/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LibraryApp.Data
+{
+    public class Database
+    {
+        private const string ConnStr = "Server=localhost;Database=librarydb;Uid=app;Pwd=Kien2005@;SslMode=None;AllowPublicKeyRetrieval=True;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnStr);
+        }
+    }
+}
+
